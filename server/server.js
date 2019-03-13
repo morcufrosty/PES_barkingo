f314d1d98d795661018d45cc9328abfa00ebb461
Max Ros i Morejon <mros33@gmail.com>
2019-03-13 16:53:01 +0100

    Servidor creat

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
-import 'express';
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 80;        // set our port
+
+var router = require('./routes.js');
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
