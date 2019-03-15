78339e8b2b3559a6992f6ce78c3f971879180bdd
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-03-15 15:59:18 +0100

    Instal·la llibreries d'encriptació

--- server/queries.js
+++ server/queries.js
@@ -1,75 +1,77 @@
 const Pool = require('pg').Pool;
 const creds = require('./creds.json');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const uuidv4 = require('uuid/v4');
 const pool = new Pool({
     // DB credentials i config
     ...creds.db,
 });
 
-const createUser = (request, response) => {
+const createUser = async (request, response) => {
     try {
         const { password, username, name } = request.body;
         const client = await pool.connect();
         await client.query('BEGIN');
         const pwd = await bcrypt.hash(password, 5);
-        await JSON.stringify(client.query('SELECT id FROM "users" WHERE "email"=$1', [username],
-            (err, result) => {
+        await JSON.stringify(
+            client.query('SELECT id FROM "users" WHERE "email"=$1', [username], (err, result) => {
                 if (result.rows[0]) {
-                    response.json({'result': 'error', 'msg': 'User already exists'});
+                    response.json({ result: 'error', msg: 'User already exists' });
                 } else {
-                    client.query('INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)', [uuidv4(), name, username, pwd],
-                        (err, result) => {
-                            if (err){
-                                console.log(err);
-                            } else {
-                                client.query('COMMIT');
-                                console.log(result);
-                                response.json({'result': 'success', 'msg': 'User created successfully'});
-                                return;
-                            }
-                        });
-                }
-            }));
-    } catch (e) {
-        console.log(e.stack)
-    }
-};
-
-async const loginUser = (request, response) => {
-    const { username, password } = request.body;
-    loginAttempt();
-    async function loginAttempt() {
-        const client = await pool.connect()
-        try {
-            await client.query('BEGIN')
-            var currentAccountsData = await JSON.stringify(client.query('SELECT id, name, email, password FROM users WHERE email=$1', [username],
-            (err, result) => {
-                if (err) {
-                    response.json({ 'result':'error' , 'msg':'error' });
-                }
-                if(result.rows[0] == null){
-                    response.json({ 'result':'error' , 'msg':'Oops. Incorrect login details.' });
-                } else{
-                    bcrypt.compare(password, result.rows[0].password, function(err, check) {
-                        if (err){
-                            response.json({ 'result':'error' , 'msg':'Error while checking password' });
-                        } else if (check){
-                            response.json({ 'result':'success' , 'msg': {'email': result.rows[0].email, 'name': result.rows[0].name} });
-                        } else{
-                            response.json({ 'result':'error' , 'msg':'Oops. Incorrect login details.' });
+                    client.query('INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)', [uuidv4(), name, username, pwd], (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            client.query('COMMIT');
+                            console.log(result);
+                            response.json({ result: 'success', msg: 'User created successfully' });
+                            return;
                         }
                     });
                 }
-            }));
-        } catch(e) {
-            throw (e);
+            })
+        );
+    } catch (e) {
+        console.log(e.stack);
+    }
+};
+
+const loginUser = async (request, response) => {
+    const { username, password } = request.body;
+    loginAttempt();
+    async function loginAttempt() {
+        const client = await pool.connect();
+        try {
+            await client.query('BEGIN');
+            var currentAccountsData = await JSON.stringify(
+                client.query('SELECT id, name, email, password FROM users WHERE email=$1', [username], (err, result) => {
+                    if (err) {
+                        response.json({ result: 'error', msg: 'error' });
+                    }
+                    if (result.rows[0] == null) {
+                        response.json({ result: 'error', msg: 'Oops. Incorrect login details.' });
+                    } else {
+                        bcrypt.compare(password, result.rows[0].password, function(err, check) {
+                            if (err) {
+                                response.json({ result: 'error', msg: 'Error while checking password' });
+                            } else if (check) {
+                                response.json({ result: 'success', msg: { email: result.rows[0].email, name: result.rows[0].name } });
+                            } else {
+                                response.json({ result: 'error', msg: 'Oops. Incorrect login details.' });
+                            }
+                        });
+                    }
+                })
+            );
+        } catch (e) {
+            throw e;
         }
     }
 };
 
 module.exports = {
-    createUser, loginUser
+    createUser,
+    loginUser,
 };
 
 // ref: https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8
