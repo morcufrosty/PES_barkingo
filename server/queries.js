9ba1b00abd91c1f3574808bc276eb325860d3426
Max Ros i Morejon <mros33@gmail.com>
2019-03-20 16:40:53 +0100

    Arregla la query de login d'usuari

--- server/queries.js
+++ server/queries.js
@@ -49,19 +49,19 @@ const createUser = async (request, response) => {
 };
 
 const loginUser = async (request, response) => {
-    const { email, password } = request.body;
+    const { email, password } = request.query;
     loginAttempt();
     async function loginAttempt() {
         const client = await pool.connect();
         try {
             await client.query('BEGIN');
             var currentAccountsData = await JSON.stringify(
-                client.query('SELECT id, name, email, password FROM users WHERE email=$1', [email], (err, result) => {
+                client.query('SELECT * FROM users WHERE email=$1;', [email], (err, result) => {
                     if (err) {
                         response.json({ result: 'error', msg: 'error' });
                     }
                     if (result.rows[0] == null) {
-                        response.json({ result: 'error', msg: 'Oops. Incorrect login details.' });
+                        response.json({ result: 'error', msg: 'Oops. User not found.' });
                     } else {
                         bcrypt.compare(password, result.rows[0].password, function(err, check) {
                             if (err) {
@@ -69,7 +69,7 @@ const loginUser = async (request, response) => {
                             } else if (check) {
                                 response.json({ result: 'success', msg: { email: result.rows[0].email, name: result.rows[0].name } });
                             } else {
-                                response.json({ result: 'error', msg: 'Oops. Incorrect login details.' });
+                                response.json({ result: 'error', msg: 'Oops. Incorrect password' });
                             }
                         });
                     }
