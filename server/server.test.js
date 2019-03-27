ec5ebee6f6de68a344545208db43282d640ee9e0
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-03-27 19:19:43 +0100

    Afegit test de POST amb base de dades postgres al travis

--- server/server.test.js
+++ server/server.test.js
@@ -5,21 +5,22 @@ const request = require('supertest');
 
 const initServer = () => {
     const app = express();
-    app.use(server);
+    app.use('/api', server);
     return app;
 };
 
-describe('GET /api', () => {
-    beforeEach(() => {
-        moxios.install();
+describe('Root path', () => {
+    test('It should respond the greetings message', async () => {
+        const response = await request(initServer()).get('/api');
+        expect(response.body.message).toBe('Welcome to the Barkingo API!');
     });
-    afterEach(() => {
-        moxios.uninstall();
-    });
-    test('It ', async () => {
-        moxios.stubRequest('/api', { message: 'Welcome to the Barkingo API!' });
-        const app = initServer();
-        await request(app).get('/api');
-        expect(moxios.requests.mostRecent().message).toBe('Welcome to the Barkingo API!');
+});
+
+describe('POST /api/register', () => {
+    test('It should response the GET method', async () => {
+        const response = await request(initServer())
+            .post('/api/register')
+            .send({ password: 'test', email: 'mail@mail.com', name: 'Test Account' });
+        expect(response.body.result).toBe(true);
     });
 });
