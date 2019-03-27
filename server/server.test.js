49934de34e15140879cc342651b983ffea57b5b2
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-03-27 16:34:05 +0100

    Afegeix primer test de servidor i rutes de renovació de token d'inici de sessió per tercers

---
+++ server/server.test.js
@@ -0,0 +1,25 @@
+const server = require('./routes');
+const express = require('express');
+const moxios = require('moxios');
+const request = require('supertest');
+
+const initServer = () => {
+    const app = express();
+    app.use(server());
+    return app;
+};
+
+describe('GET /api', () => {
+    beforeEach(() => {
+        moxios.install();
+    });
+    afterEach(() => {
+        moxios.uninstall();
+    });
+    test('It ', async () => {
+        moxios.stubRequest('/api', { message: 'Welcome to the Barkingo API!' });
+        const app = initServer();
+        await request(app).get('/api');
+        expect(moxios.requests.mostRecent().message).toBe('Welcome to the Barkingo API!');
+    });
+});
