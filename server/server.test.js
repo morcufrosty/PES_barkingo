a5a123c692355d4cf6bbc9e2a1149b901d33bd4d
Arnau Buch <arnau.buch@est.fib.upc.edu>
2019-03-27 22:05:02 +0100

    Arregla el test de registre API

--- server/server.test.js
+++ server/server.test.js
@@ -1,13 +1,19 @@
 const server = require('./routes');
 const express = require('express');
-const moxios = require('moxios');
 const request = require('supertest');
+var bodyParser = require('body-parser');
 
 const initServer = () => {
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
     app.use('/api', server);
     return app;
 };
+
+afterAll(async () => {
+    await new Promise(resolve => setTimeout(() => resolve(), 500)); // avoid jest open handle error
+});
 
 describe('Root path', () => {
     test('It should respond the greetings message', async () => {
@@ -20,7 +26,9 @@ describe('POST /api/register', () => {
     test('It should response the GET method', async () => {
         const response = await request(initServer())
             .post('/api/register')
-            .send({ password: 'test', email: 'mail@mail.com', name: 'Test Account' });
-        expect(response.body.result).toBe(true);
+            .send({ password: 'test', email: 'mail@mail.com', name: 'Test Account' })
+            .set('Accept', /application\/json/)
+            .type('form')
+        expect(response.body.success).toBe(true);
     });
 });
