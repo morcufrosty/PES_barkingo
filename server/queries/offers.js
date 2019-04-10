6fba2be8b505fc65393473a0e121bcda008e647e
Max Ros i Morejon <mros33@gmail.com>
2019-04-10 16:47:47 +0200

    afageix query per a crear una nova oferta

--- server/queries/offers.js
+++ server/queries/offers.js
@@ -21,6 +21,30 @@ const getOffers = (request, response) => {
     })
 }
 
+const createOffer = (request, response) => {
+    const { name, type, species, race, sex, age, description, iniDate, endDate } = request.body || request.query;
+    await pool.connect(async (err, client, done) => {
+        if (err) {
+            response.json({ success: false, msg: 'Error accessing the database' });
+            done();
+            return;
+        }
+        await client.query('BEGIN');
+        if(description === undefined) description = "null";
+        if(iniDate === undefined) iniDate = "null";
+        if(endDate === undefined) endDate = "null";
+        await client.query('INSERT INTO animals (id, name, type, species, race, sex, age, description, iniDate, endDate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [uuidv4(), name, type, species, race, sex, age, description, iniDate, endDate], (err, result) => {
+            if (err) {
+                console.error('Unknown error', err);
+            } else {
+                client.query('COMMIT');
+                response.json({ success: true, msg: 'Offer created successfully' });
+            }
+        });
+    })
+}
+
 module.exports = {
     getOffers,
+    createOffer,
 }
