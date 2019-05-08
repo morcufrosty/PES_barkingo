275875f79663873287573e03aec51b8de42155fb
jamipuchi <jaumepuigturon4@gmail.com>
2019-05-08 16:49:30 +0200

    Fixed filter stuff

--- app/Components/filter.js
+++ app/Components/filter.js
@@ -23,7 +23,7 @@ export default class filter extends React.Component {
                 <Text style={{ color: 'white', fontSize: 45, flex: 1 }}>Register</Text>
                 <View style={{ flex: 1 }}>
                     <Text style={{ color: 'white' }}>{"Rang d'edat"}</Text>
-                    <View style={{flexDirection: row}} >
+                    <View style={{flexDirection: 'row'}} >
                         <Text style={{ color: 'white' }}>{"De:"}</Text>
                         <TextInput keyboardType='numeric' style={{width:'30%', backgroundColor: 'white', opacity: 0.5, borderRadius: 5, paddingVertical: 0, height: 35 }}></TextInput>
                         <Text style={{ color: 'white' }}>{"Fins:"}</Text>
