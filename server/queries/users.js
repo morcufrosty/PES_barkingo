f05e4087be8b267898f737e24f59c705bcb89002
Max Ros i Morejon <mros33@gmail.com>
2019-05-15 16:00:15 +0200

    Afageix funcionalitat a deleteUser

--- server/queries/users.js
+++ server/queries/users.js
@@ -137,7 +137,43 @@ const updateUser = async (request, response) => {
 }
 
 deleteUser = async (request, response) => {
-    response.json({ success: false, msg: 'Not implemented yet getUserImage' });
+    //response.json({ success: false, msg: 'Not implemented yet getUserImage' });
+    const { email, name } = request.decoded;
+    const { id: idUser } = request.params;
+    await pool.connect(async (err, client, done) => {
+        if (err) {
+            response.json({ success: false, msg: 'Error accessing the database' });
+            done();
+            return;
+        }
+        await client.query('BEGIN');
+        await client.query(
+            'SELECT id FROM users WHERE email=$1 AND name=$2;', [email, name],
+            (err, result) => {
+                if (err || result.rowCount == 0) {
+                    console.log(err)
+                    response.json({ success: false, msg: 'User ' + email + ' doesn\'t exist' });
+                } else {
+                    if(result.rows[0].id == idUser) {
+                        client.query(
+                        'DELETE FROM users WHERE "id"=$1;', [idUser],
+                        (error, res) => {
+                            if (error) {
+                                console.error('Unknown error', error);
+                                response.json({ success: false, msg: 'Unknown error' });
+                            } else {
+                                client.query('COMMIT');
+                                response.json({ success: true, msg: 'User deleted successfully', id: idUser });
+                            }
+                        });
+                    }
+                    else {
+                        response.json({ success: false, msg: 'No authorized for operation' });
+                    }
+                }
+            });
+        done();
+    })
 }
 
 const getUserImage = async (request, response) => {
