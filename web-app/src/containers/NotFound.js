d914f291f1e0b4dd4b09ca2deffee20f08084fc9
jamipuchi <jaumepuigturon4@gmail.com>
2019-05-17 15:26:15 +0200

    Update web-app

---
+++ web-app/src/containers/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class NotFound extends Component {
+
+  render() {
+    return (
+      <div>
+          <h1>404 Not found</h1>
+      </div>
+    );
+  }
+}
