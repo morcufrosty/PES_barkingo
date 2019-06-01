24a5170afc2aab7d650b16a6d7061f9d3ede184d
jamipuchi <jaumepuigturon4@gmail.com>
2019-06-01 11:53:19 +0200

    Update web-app

--- web-app/src/containers/Animals.js
+++ web-app/src/containers/Animals.js
@@ -1,51 +1,105 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Card, Button, ListGroup, ListGroupItem, Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
 
-const numbers = [1,2,3,4,5,6,7,8,9]; 
 
 
-const updatedNums = numbers.map((number)=>{ 
-  return (
-    <div class='m-3'>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
-        <Card.Body>
-          <Card.Title>Nom gos {number}</Card.Title>
-          <Card.Text>
-            Aquí aniria la descripció del puto gos.
-          </Card.Text>
-        </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroup.Item variant="danger">Reports: {number}</ListGroup.Item>
-          <ListGroupItem>Sexe</ListGroupItem>
-          <ListGroupItem>Edat</ListGroupItem>
-          <ListGroupItem>Raça</ListGroupItem>
-          <ListGroupItem>Nom del puto usuari</ListGroupItem>
-        </ListGroup>
-        <Card.Body>
-          <Button variant="danger">Eliminar anunci</Button>
-        </Card.Body>
-      </Card>
-    </div>
-  ); 
-}); 
 
-function Animals() {
+export default class Animals extends Component {
 
-  return (
-    <div>
-      <div class='m-3 row fixed'>
-      </div>
-      <div>
-        <div class="m-3 row">
-          {updatedNums}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      animalOffers: new Array(),
+    };
+    this.getOffersRendered = this.getOffersRendered.bind(this);
+  }
+
+  async getDogOffers() {
+    return fetch('http://10.4.41.164/api/offers/all', {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'x-access-token': localStorage.getItem("api_token")
+        }
+    }).then((response) => response.json())
+        .then((responseJson) => {
+            return responseJson.offers;
+        }).catch((error) => {
+            console.error(error);
+        });
+  }
+
+  async getImageFromServer(id) {
+    return fetch(`http://10.4.41.164/api/offers/${id}/image`, {
+      method: 'GET',
+      headers: {
+        Accept: '*',
+        'x-access-token': localStorage.getItem("api_token")
+      }
+      }).then((response =>  {
+        return response.text();
+      }))
+  }
+
+  async getOffers(){
+    var dogOffers = await this.getDogOffers();
+    console.log(dogOffers);
+    return dogOffers;
+  }
+  
+  componentWillMount(){
+    this.setState({animalOffers:this.getOffers()}) ;//({animalOffers: this.getOffers()});
+  }
+
+  getOffersRendered(){
+    var ofertes = []
+    console.log(this.state.animalOffers.length != undefined);
+    if (this.state.animalOffers.length != undefined) ofertes = this.state.animalOffers;
+    return ofertes.map((animal)=>{ 
+      return (
+        <div class='m-3'>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={this.getImageFromServer(1)} />
+            <Card.Body>
+              <Card.Title>{animal.name}</Card.Title>
+              <Card.Text>
+                De mom no està implementat
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item variant="danger">Reports: Num reports</ListGroup.Item>
+              <ListGroupItem>{animal.sex}</ListGroupItem>
+              <ListGroupItem>{animal.age}</ListGroupItem>
+              <ListGroupItem>{animal.race}</ListGroupItem>
+              <ListGroupItem>Nom del puto usuari</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Button variant="danger">Eliminar anunci</Button>
+            </Card.Body>
+          </Card>
         </div>
-      </div>
-      
-    </div>
-  );
-}
+      ); 
+    });
+  }
+   
+  
 
-export default Animals;
+  render(){
+    return (
+      <div>
+        <div class='m-3 row fixed'>
+        </div>
+        <div>
+          <div class="m-3 row">
+            {this.getOffersRendered()}
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
